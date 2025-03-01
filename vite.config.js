6db0a7a1a32f0820a7c302b3129b602c0d355d1d
chris chen <kjh88126@gmail.com>
2025-03-01 17:30:38 +0800

    fix:vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/React-main-7/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-main-7/' : '/',
   plugins: [react()],
 })
